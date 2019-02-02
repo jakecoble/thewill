5f48333528487533f2049d9d9863d5c28e92e80c
Jake Coble <jakecoble@users.noreply.github.com>
2019-02-02 15:07:48 -0800

    fix up some things

--- src/components/Stat.vue
+++ src/components/Stat.vue
@@ -5,7 +5,7 @@
 </template>
 
 <style scoped>
- .stat {
-   color: red;
- }
+  .stat {
+    color: red;
+  }
 </style>
--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,16 +1,28 @@
 <template>
   <ul class="stats">
-    <Stat v-for="stat in stats">{{ stat.display_name }}: {{ stat.value }}</Stat>
+    <!-- FIXME: We need to sanitize the display name before using it as a key. -->
+    <Stat v-for="stat in stats" :key="stat.display_name">
+      {{ stat.display_name }}: {{ stat.value }}
+    </Stat>
   </ul>
 </template>
 
 <script>
- import { mapState } from 'vuex';
- import Stat from './Stat';
+  import { mapState } from 'vuex'
+  import Stat from './Stat.vue'
 
- export default {
-   computed: mapState({
-     stats: store => store.stats
-   })
- }
+  export default {
+    computed: mapState({
+      stats: store => store.stats
+    }),
+
+    components: {
+      Stat
+    }
+  }
 </script>
+
+<style scoped>
+  .stats {
+  }
+</style>
