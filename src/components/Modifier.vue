838329acec66a2a1efcd6e2f740e2ec747c34246
Jake Coble <jakecoble@users.noreply.github.com>
2019-04-24 13:54:15 -0700

    improve modifier template, add modifier component

---
+++ src/components/Modifier.vue
@@ -0,0 +1,30 @@
+<template functional>
+  <li class="modifier">
+    <div v-if="!props.mod.hidden" class="name hidden">???</div>
+    <div v-else class="name">{{ props.mod.display_name }}</div>
+    <ul class="statBonuses">
+      <li v-if="props.mod.will" class="statBonus">
+        <div class="statBonusName">will</div>
+        <div class="statBonusValue">{{ props.mod.will }}</div>
+      </li>
+
+      <li v-if="props.mod.org" class="statBonus">
+        <div class="statBonusName">org</div>
+        <div class="statBonusValue">{{ props.mod.org }}</div>
+      </li>
+
+      <li v-if="props.mod.health" class="statBonus">
+        <div class="statBonusName">health</div>
+        <div class="statBonusValue">{{ props.mod.health }}</div>
+      </li>
+
+      <li v-if="props.mod.hygiene" class="statBonus">
+        <div class="statBonusName">hygiene</div>
+        <div class="statBonusValue">{{ props.mod.hygiene }}</div>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<style scoped>
+</style>
--- src/components/Modifiers.vue
+++ src/components/Modifiers.vue
@@ -1,16 +1,19 @@
 <template>
   <ul class="modifiers">
-    <li v-for="mod in activeMods" v-bind:key="mod.display_name">
-      {{ mod.hidden ? '???' : mod.display_name }} | org {{ mod.org }}, hygiene {{ mod.hygiene }}, health {{ mod.health }}, will {{ mod.will }}
-    </li>
+    <Modifier v-for="mod in activeMods" :mod="mod" :key="mod.display_name" />
   </ul>
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
 
+  import Modifier from './Modifier.vue'
+
   export default {
     name: 'Modifiers',
+    components: {
+      Modifier
+    },
 
     computed: mapGetters('modifiers', [
       'activeMods'
